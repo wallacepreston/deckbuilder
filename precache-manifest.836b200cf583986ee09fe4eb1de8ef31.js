bdf3dbfc6af57205a17aafd1019b6c19286a0d6a
Preston Wallace <wallace.preston@gmail.com>
2020-06-11 21:13:44 -0700

    Updates

---
+++ precache-manifest.836b200cf583986ee09fe4eb1de8ef31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15e36b8d3e9ff93052cd9f0c6f10c8c",
+    "url": "/deckbuilder/index.html"
+  },
+  {
+    "revision": "5cdefe5bdee6fc06574e",
+    "url": "/deckbuilder/static/css/main.60b5589c.chunk.css"
+  },
+  {
+    "revision": "b738755e1cb9393ae5d4",
+    "url": "/deckbuilder/static/js/2.52919355.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deckbuilder/static/js/2.52919355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdefe5bdee6fc06574e",
+    "url": "/deckbuilder/static/js/main.b47ef102.chunk.js"
+  },
+  {
+    "revision": "c7937e306ab6267c2825",
+    "url": "/deckbuilder/static/js/runtime-main.aba5058e.js"
+  }
+]);
